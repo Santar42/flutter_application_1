7ea55917f86ba7a49bdc5dcb9c70e3f4baaf258b
Santar42 <61143217@g.cmru.ac.th>
2021-03-07 13:35:46 +0700

    Start

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screen/example.dart';
+import 'package:flutter_application_1/screen/form_screen.dart';
+import 'package:flutter_application_1/screen/list_view_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({Key key}) : super(key: key);
@@ -12,43 +15,55 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Container(
        child: Scaffold(
-      drawer: Drawer(
+        drawer: Drawer(
         child: Center(
           child: Text("Hello drawer"),
         ),
       ),
       appBar: AppBar(
-        title: Text("my first project"),
+        title: Text("My First Project"),
       ),
       body: Center(
         child: Column(
           children: [
             Container(
               color: Colors.red,
-              child: Text("aaaaaaa;bbbbbb")),
+              child: Text("สวัสดีชาวโลก")),
             Container(
               width: 150,
               height:150,
               color: Colors.brown[400],
               child: Image.asset('assets/images/ss.jpg')),
-          ],
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: "home",
+        ElevatedButton(
+          onPressed: (){
+            Navigator.push(context, MaterialPageRoute(
+              builder: (context)=> ListViewScreen(),
+              ),
+              );
+          }, 
+          child: Text("กดแล่"),
+          ),  
+        TextButton(
+          onPressed: () {
+             Navigator.push(context, MaterialPageRoute(
+              builder: (context)=> FormScreen(),
+              ),
+              );
+          },
+          child: Text("ลองกดดู"),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.phone),
-            label: "phone",
+        FlatButton(
+          onPressed: () {
+             Navigator.push(context, MaterialPageRoute(
+              builder: (context)=> Example(),
+              ),
+              );
+          },
+          child: Text("Button"),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.people),
-            label: "people",
-          )
+
         ],
+        ),
       ),
     ),
     );
